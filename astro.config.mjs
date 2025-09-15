c706a1064582388460f8a95d1f25094328ed18d8
marzsp <mariannepeterson92@gmail.com>
2025-09-15 15:03:00 +0200

    astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://marzsp.github.io/DevNotes/',
+	site: 'https://marzsp.github.io/DevNotes',
+	base: '/DevNotes',
 	integrations: [mdx(), sitemap()],
 });

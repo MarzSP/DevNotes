4cdf0e99816ae52f272073136c56373b5b9e9e4b
marzsp <mariannepeterson92@gmail.com>
2025-09-15 14:37:17 +0200

    consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'DevNotes';
+export const SITE_DESCRIPTION = 'Documenting the journey from ‘hello world’ to high-level systems';
